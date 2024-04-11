d1fe2d490045fc6297d061cc7c8d5290a25891fe
MattSeminaro1 <mattseminaro1@gmail.com>
2024-04-10 23:05:38 -0500

    multiple changes

---
+++ data_collector/main.py
@@ -0,0 +1,164 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from datetime import datetime, timedelta
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import psycopg2
+from psycopg2 import sql
+
+
+# Get the current date
+current_date = datetime.now()
+
+# Subtract one day from the current date
+previous_date = current_date - timedelta(days=1)
+
+# Extract year, month, and day from the previous date
+year = previous_date.year
+month = previous_date.month
+day = previous_date.day
+def find_endpoints(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        all_links = soup.find_all('a')
+        endpoints = [urljoin(url, link.get('href')) for link in all_links if link.get('href') and '/boxes' in link.get('href') and link.get('href').endswith('.shtml')]
+        return endpoints
+    else:
+        print("Failed to retrieve data")
+        return None
+# URL of the page containing the box scores
+url = f"https://www.baseball-reference.com/boxes/?year={year}&month={month}&day={day}"
+# Find endpoints
+endpoints = find_endpoints(url)
+if endpoints:
+    print("List of endpoints:")
+    for endpoint in endpoints:
+        print(endpoint)
+else:
+    print("No endpoints found.")
+
+print('done')
+
+
+###Need to edit this function
+def get_stat_data(link_list, type):
+    soup_list = []
+    for i in link_list:
+        driver = webdriver.Chrome()
+        driver.get(i)
+        time.sleep(5)
+
+        if type == 'batting':
+            stat_elements = ["/html[1]/body[1]/div[2]/div[4]/div[5]/div[1]/div[1]/ul[1]/li[1]",
+                             "/html[1]/body[1]/div[2]/div[4]/div[6]/div[1]/div[1]/ul[1]/li[1]"]
+        else:
+            stat_elements = ["/html[1]/body[1]/div[2]/div[4]/div[8]/div[3]/div[1]/div[1]/div[1]/ul[1]/li[1]",
+                             "/html[1]/body[1]/div[2]/div[4]/div[8]/div[3]/div[2]/div[1]/div[1]/ul[1]/li[1]"]
+        i = 0
+        for element in stat_elements:
+            button = driver.find_element(By.XPATH, element)
+            driver.execute_script("arguments[0].scrollIntoView({behavior: 'instant', block: 'center'});", button)
+            time.sleep(2)
+            button.click()
+            element = element + "/div/ul/li[3]/button"
+            time.sleep(1)
+            driver.find_element(By.XPATH, element).click()
+            time.sleep(2)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            if type == 'batting':
+                elements = soup.find_all(
+                    lambda tag: tag.has_attr('id') and tag['id'].startswith('csv') and tag['id'].endswith('batting'))
+            else:
+                elements = soup.find_all(
+                    lambda tag: tag.has_attr('id') and tag['id'].startswith('csv') and tag['id'].endswith('pitching'))
+            csv = elements[i].text.split("\n")
+            i = i + 1
+            soup_list.append(csv)
+        print('done')
+    return soup_list
+raw_batting_data = get_stat_data(endpoints, 'batting')
+raw_pitching_data = get_stat_data(endpoints, 'pitching')
+print('done')
+
+
+def clean_csv(csv, type):
+    no_blank = []
+    for i in csv:
+        for j in i:
+            if j != '':
+                no_blank.append(j)
+        new_data_split = []
+    for i in no_blank:
+        new_line = pd.DataFrame(i.split(','))
+        new_data_split.append(new_line)
+    season_data = pd.concat(new_data_split, ignore_index = True, axis = 1).T
+    header = season_data.iloc[2]
+    season_data = season_data[3:]
+    season_data.columns = header
+    if type == 'batting':
+        season_data = season_data[
+            (season_data['AB'].notnull()) & (season_data['Batting'] != 'Team Totals') & (season_data['AB'] != '')
+            & (season_data['Batting'] != 'Batting') & (season_data['R'].notnull())]
+    else:
+        season_data = season_data[
+            (season_data['IP'].notnull()) & (season_data['Pitching'] != 'Team Totals') & (season_data['IP'] != '')
+            & (season_data['Pitching'] != 'Pitching') & (season_data['H'].notnull())]
+    season_data.replace('', pd.NA, inplace=True)
+    season_data['cWPA'] = season_data['cWPA'].str.replace('%', '')
+    return season_data
+
+pitching_data_csv = clean_csv(raw_pitching_data, 'pitching')
+pitching_data_csv['upload_dttm'] = previous_date
+batting_data_csv = clean_csv(raw_batting_data, 'batting')
+batting_data_csv['upload_dttm'] = previous_date
+pitching_data_csv.rename(columns={col: col.lower() for col in pitching_data_csv.columns}, inplace=True)
+batting_data_csv.rename(columns={col: col.lower() for col in  batting_data_csv.columns}, inplace=True)
+batting_data_csv.rename(columns={'wpa+': 'wpaplus', 'wpa-': 'wpaminus'}, inplace=True)
+
+
+##insert data
+conn_params = {
+    'dbname': 'db_init',
+    'user': 'csca5028',
+    'password': 'csca5028',
+    'host': 'csca5028-db-instance.c1a04q2cyd4h.us-east-1.rds.amazonaws.com',
+    'port': '5432'
+}
+
+conn = psycopg2.connect(**conn_params)
+cur = conn.cursor()
+
+# Insert for Pitching Data
+table_name = 'pitching_data'
+columns = pitching_data_csv.columns.tolist()
+insert_statement = sql.SQL("INSERT INTO {} ({}) VALUES ({});").format(
+    sql.Identifier(table_name),
+    sql.SQL(', ').join(map(sql.Identifier, columns)),
+    sql.SQL(', ').join(sql.Placeholder() * len(columns))
+)
+pitching_data_csv = pitching_data_csv.where(pd.notnull(pitching_data_csv), None)
+data = [tuple(x) for x in pitching_data_csv.to_numpy()]
+cur.executemany(insert_statement, data)
+conn.commit()
+
+# Insert for Batting Data
+table_name = 'batting_data'
+columns = batting_data_csv.columns.tolist()
+insert_statement = sql.SQL("INSERT INTO {} ({}) VALUES ({});").format(
+    sql.Identifier(table_name),
+    sql.SQL(', ').join(map(sql.Identifier, columns)),
+    sql.SQL(', ').join(sql.Placeholder() * len(columns))
+)
+batting_data_csv = batting_data_csv.where(pd.notnull(batting_data_csv), None)
+data = [tuple(x) for x in batting_data_csv.to_numpy()]
+cur.executemany(insert_statement, data)
+conn.commit()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+print('done')
--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+# Define routes and view functions
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route('/select/<choice>')
+def select(choice):
+    if choice == 'pitching':
+        return redirect(url_for('pitching'))
+    elif choice == 'batting':
+        return redirect(url_for('batting'))
+    else:
+        return "Invalid choice"
+
+@app.route('/pitching')
+def pitching():
+    # Code for pitching endpoint
+    return "Pitching data"
+
+@app.route('/batting')
+def batting():
+    # Code for batting endpoint
+    return "Batting data"
+
+if __name__ == '__main__':
+    app.run(debug=True)
