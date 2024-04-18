c435f85160876f22987b8eaaf8a1f89acb26ccb8
MattSeminaro1 <mattseminaro1@gmail.com>
2024-04-17 22:06:36 -0500

    trying to get seleium schedueled

--- data_collector/main.py
+++ data_collector/main.py
@@ -51,12 +51,11 @@ print('done')
 def get_stat_data(link_list, type):
     soup_list = []
     for i in link_list:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get(i)
         time.sleep(5)
 
